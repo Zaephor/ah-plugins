6170f710023c2280585ad741ec9e277a1746d4e1
Zaephor <Zaephor@users.noreply.github.com>
2019-06-09 22:17:31 -0700

    chore(ah-objection-plugin): reorganizing things, initial commit

---
+++ modules/ah-objection-plugin/README.md
@@ -0,0 +1,11 @@
+# `@zaephor-ah/ah-objection-plugin`
+
+> TODO: description
+
+## Usage
+
+```
+const ahObjectionPlugin = require('ah-objection-plugin');
+
+// TODO: DEMONSTRATE API
+```
---
+++ modules/ah-objection-plugin/initializers/ah-objection-plugin.js
@@ -0,0 +1,44 @@
+'use strict'
+const { Initializer, api } = require('actionhero')
+const path = require('path')
+const glob = require('glob')
+const { Model } = require('objection')
+
+module.exports = class ObjectionInitializer extends Initializer {
+  constructor () {
+    super()
+    this.name = 'ah-objection-plugin'
+    this.loadPriority = 1001
+    this.startPriority = 1001
+    this.stopPriority = 1001
+  }
+
+  async initialize () {
+    api.models = {}
+    api.objection = {
+      Model: Model
+    }
+    api.log('[' + this.loadPriority + '] ' + this.name + ': Initializing')
+
+    // TODO: Throw an error when api.knex isn't loaded
+    api.objection.Model.knex(api.knex)
+
+    api.objection.loadFile = async (fileType, file) => {
+      // console.log({ fileType, file })
+      if (fileType === 'models') {
+        // const tmpModel = api.sequelize['import'](path.resolve(file))
+        let tmpModel = require(path.resolve(file))(api.objection.Model)
+        if (api[fileType][tmpModel.tableName]) { throw new Error('[' + this.name + '] The model "' + tmpModel.tableName + '" in ' + file + ' was already defined.') }
+        api[fileType][tmpModel.tableName] = tmpModel
+      }
+    }
+
+    for (let pluginName in api.config.plugins) {
+      if (api.config.plugins[pluginName].models !== false) {
+        let pluginPath = api.config.plugins[pluginName].path
+        let files = glob.sync(path.join(pluginPath, 'models', '**', '*.js'))
+        for (let j in files) { await api.objection.loadFile('models', files[j]) }
+      }
+    }
+  }
+}
