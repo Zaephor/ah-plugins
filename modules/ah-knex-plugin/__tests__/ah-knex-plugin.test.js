068280fd7804056fd3182d4cb04044f10fadf18c
Zaephor <Zaephor@users.noreply.github.com>
2019-06-19 22:38:34 -0700

    test(ah-knex-plugin): rough initial test if knex was loaded

--- modules/ah-knex-plugin/__tests__/ah-knex-plugin.test.js
+++ modules/ah-knex-plugin/__tests__/ah-knex-plugin.test.js
@@ -1,22 +1,30 @@
 /* eslint-env node, mocha */
+process.env.PROJECT_ROOT = path.join(require.resolve('actionhero'), '..')
+
 const path = require('path')
-
-process.env.PROJECT_ROOT = path.join(__dirname, '..', 'node_modules', 'actionhero')
-
+const {expect} = require('chai')
 const ActionHero = require('actionhero')
 const actionhero = new ActionHero.Process()
+
+let config = require(path.join(__dirname, '..', 'config', 'ah-knex-plugin.js'))
+let environment = (process.env.NODE_ENV && config[process.env.NODE_ENV]) ? process.env.NODE_ENV : 'default'
 let api
 
 describe('ah-knex-plugin', () => {
-  before(async () => {
-    let configChanges = {
-      'ah-knex-plugin': { },
-      plugins: {
-        'ah-knex-plugin': { path: path.join(__dirname, '..') }
-      }
+  let configChanges = {
+    'ah-knex-plugin': config[environment]['ah-knex-plugin'](ActionHero.api),
+    plugins: {
+      'ah-knex-plugin': {path: path.join(__dirname, '..')}
     }
-    api = await actionhero.start({ configChanges })
+  }
+
+  before(async () => {
+    api = await actionhero.start({configChanges})
   })
 
   after(async () => { await actionhero.stop() })
+
+  it('knex should be in api scope', async () => {
+    expect(api.knex).to.exist
+  })
 })
