171177e6d6f47e4dfd60e05237515db6cdc3284b
Zaephor <Zaephor@users.noreply.github.com>
2019-06-20 19:52:55 -0700

    test(ah-objection-plugin): initial testing that objection loaded

--- modules/ah-objection-plugin/__tests__/ah-objection-plugin.test.js
+++ modules/ah-objection-plugin/__tests__/ah-objection-plugin.test.js
@@ -1,22 +1,38 @@
 /* eslint-env node, mocha */
 const path = require('path')
-
-process.env.PROJECT_ROOT = path.join(__dirname, '..', 'node_modules', 'actionhero')
-
+const {expect} = require('chai')
 const ActionHero = require('actionhero')
 const actionhero = new ActionHero.Process()
+
+process.env.PROJECT_ROOT = path.join(require.resolve('actionhero'), '..')
+// let config = require(path.join(__dirname, '..', 'config', 'ah-objection-plugin.js'))
+// let environment = (process.env.NODE_ENV && config[process.env.NODE_ENV]) ? process.env.NODE_ENV : 'default'
 let api
 
 describe('ah-objection-plugin', () => {
-  before(async () => {
-    let configChanges = {
-      'ah-objection-plugin': { },
-      plugins: {
-        'ah-objection-plugin': { path: path.join(__dirname, '..') }
-      }
+  let configChanges = {
+    // 'ah-objection-plugin': config[environment]['ah-objection-plugin'](ActionHero.api),
+    plugins: {
+      'ah-objection-plugin': {path: path.join(__dirname, '..')}
     }
-    api = await actionhero.start({ configChanges })
+  }
+
+  before(async () => {
+    api = await actionhero.start({configChanges})
   })
 
   after(async () => { await actionhero.stop() })
+
+  it('ActionHero server launches', () => {
+    expect(api.running).to.equal(true)
+  })
+
+  Array('knex', 'objection', 'models').forEach(function (attribute) {
+    it(attribute + ' should be in api scope', async () => {
+      expect(api[attribute]).to.exist
+    })
+  })
+
+  it('should validate that migrations can be detected')
+  it('should validate that migrations can be run')
 })
